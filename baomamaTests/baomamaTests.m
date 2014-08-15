1e8d2c0314ecf5438a51cbbdc3170919be26698c
bb-coder <bbcoderios@gmail.com>
2014-08-15 14:07:19 +0800

    Initial Commit

---
+++ baomamaTests/baomamaTests.m
@@ -0,0 +1,34 @@
+//
+//  baomamaTests.m
+//  baomamaTests
+//
+//  Created by bb_coder on 14-8-15.
+//  Copyright (c) 2014年 bb-coder. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface baomamaTests : XCTestCase
+
+@end
+
+@implementation baomamaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
